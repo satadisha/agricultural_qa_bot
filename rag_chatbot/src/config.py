1448e76b3b40886dd148caffb01479699d73eba4
jibekgupta <jibek.gupta@bison.howard.edu>
2025-07-29 14:27:30 -0500

    updates

--- rag_chatbot/src/config.py
+++ rag_chatbot/src/config.py
@@ -3,6 +3,8 @@ CHUNK_SIZE = 512
 # EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-base"
 
 EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-large"
+LLM_NAME = "microsoft/Phi-4-mini-instruct"
+# LLM_NAME = "MarsGray/Phi-4-mini-instruct-lora-model"
 
 RAW_PDF_DIR = "data/albania.pdf"
 JSONL_OUTPUT_DIR = "processed/jsonl"
